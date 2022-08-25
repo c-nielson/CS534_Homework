f333770c41e3e9c86a93def2c6662ae0001b6d87
Christopher Nielson <cnielson3@gmail.com>
2022-08-25 13:08:21 -0400

    Initial commit. Includes files for HW0, problems 1-2.

--- HW0.py
+++ HW0.py
@@ -1,6 +1,27 @@
 import pandas as pd
 import seaborn as sns
+from matplotlib import pyplot as plt
+
+
+def problem1():
+	df = pd.read_csv('hw0_p1.csv')
+	dfm = df.melt('y', var_name='xn', value_name='x')
+	sns.set_theme()
+	fig, axs = plt.subplots(1, 3)
+	sns.lineplot(ax=axs[0], x='x1', y='y', data=df, label='x1 v y')
+	sns.lineplot(ax=axs[0], x='x2', y='y', data=df, label='x2 v y')
+	sns.lineplot(ax=axs[0], x='x1', y='x2', data=df, label='x1 v x2')
+	sns.kdeplot(ax=axs[1], x='x', y='y', data=dfm)
+	sns.kdeplot(ax=axs[2], x='x', y='y', data=dfm, hue='xn', legend=True)
+	axs[0].legend()
+	plt.show()
+	print(df.describe())
+
+
+	def problem2():
+
+
 
 if __name__ == '__main__':
-	df = pd.read_csv('hw0_p1.csv')
-	sns.lineplot(x='x1', y='y', data=df)
+	# problem1()
+	problem2()
