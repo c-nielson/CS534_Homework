a04001ddfbbe3d2a5ffd9b8b6dd8e9cdb9e973d5
Christopher Nielson <cnielson3@gmail.com>
2022-10-18 23:25:24 -0400

    Completed HW2, including optimization.

--- HW2/HW2.py
+++ HW2/HW2.py
@@ -1,46 +1,60 @@
+import time
 import math
 import re
+import time
+
 from numpy import where
 from tkinter import filedialog as fd
 import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics import accuracy_score, f1_score
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from sklearn.svm import SVC
 import multiprocessing
 
-
-n_jobs = math.floor(multiprocessing.cpu_count() * 0.25)
+# n_jobs = math.floor(multiprocessing.cpu_count() * 0.25)
+n_jobs = 20
 
 
 def load_files():
-	train_file = fd.askopenfilename(title="Select training file", filetypes=[("CSV", "*.csv")])
-	validation_file = fd.askopenfilename(title="Select validation file", filetypes=[("CSV", "*.csv")])
-	test_file = fd.askopenfilename(title="Select testing file (with labels)", filetypes=[("CSV", "*.csv")])
+	"""
+	Loads the training and validation sets from selected files and returns the corresponding DataFrames.
+	:return: df_train, df_validation DataFrames
+	"""
+	train_file = fd.askopenfilename(title="Select Training File", filetypes=[("CSV", "*.csv")])
+	validation_file = fd.askopenfilename(title="Select File to Use for Scoring", filetypes=[("CSV", "*.csv")])
 
 	df_train = pd.read_csv(train_file)
 	df_validation = pd.read_csv(validation_file)
-	df_test = pd.read_csv(test_file)
 
-	return df_train, df_validation, df_test
+	return df_train, df_validation
 
 
-def NB(train, validation, test, vec):
-	params = {
-		'alpha': np.linspace(0, 1000, 100001)
-	}
+# Optimum Parameters:
+# 	alpha=0.09
+def NB(train, validation, vec):
+	"""
+	Used to optimize and train a Multinomial Naive Bayes model. Commented out code was previously used for parameter optimization.
+	:param train: Training DataFrame
+	:param validation: Validation DataFrame
+	:param vec: Vectorizer for tokenizing tweets
+	:return: None
+	"""
+	# params = {
+	# 	'alpha': np.linspace(0, 1000, 100001)
+	# }
 
-	nb = MultinomialNB()
-	gs = GridSearchCV(nb, params, n_jobs=n_jobs)
-	print('\nOptimizing Naive Bayes...')
-	gs.fit(vec.transform(train['tweet']), train['target'])
-	print(gs.best_params_)
-	print(gs.best_score_)
+	nb = MultinomialNB(alpha=0.09)
+	# gs = GridSearchCV(nb, params, n_jobs=n_jobs)
+	# print('\nOptimizing Naive Bayes...')
+	# gs.fit(vec.transform(train['tweet']), train['target'])
+	# print(gs.best_params_)
+	# print(gs.best_score_)
 
-	nb = gs.best_estimator_
+	# nb = gs.best_estimator_
 	nb.fit(vec.transform(train['tweet']), train['target'])
 	nb_y_pred = nb.predict(vec.transform(validation['tweet']))
 
@@ -51,20 +65,30 @@ def NB(train, validation, test, vec):
 	)
 
 
-def RF(train, validation, test, vec):
-	params = {
-		'n_estimators': np.arange(0, 1001),
-		'min_samples_leaf': np.linspace(0, 20, 101)
-	}
+# Optimum Parameters:
+# 	n_estimators: 49
+#	min_samples_leaf: 1
+def RF(train, validation, vec):
+	"""
+	Used to optimize and train a Random Forest. Commented out code was previously used for parameter optimization.
+	:param train: Training DataFrame
+	:param validation: Validation DataFrame
+	:param vec: Vectorizer for tokenizing tweets
+	:return: None
+	"""
+	# params = {
+	# 	'n_estimators': np.arange(1, 51),
+	# 	'min_samples_leaf': np.arange(11)
+	# }
 
-	rf = RandomForestClassifier(random_state=0)
-	gs = GridSearchCV(rf, params, n_jobs=n_jobs)
-	print('\nOptimizing Random Forest...')
-	gs.fit(vec.transform(train['tweet']), train['target'])
-	print(gs.best_params_)
-	print(gs.best_score_)
+	rf = RandomForestClassifier(min_samples_leaf=1, n_estimators=49)
+	# gs = GridSearchCV(rf, params, n_jobs=n_jobs)
+	# print('\nOptimizing Random Forest...')
+	# gs.fit(vec.transform(train['tweet']), train['target'])
+	# print(gs.best_params_)
+	# print(gs.best_score_)
 
-	rf = gs.best_estimator_
+	# rf = gs.best_estimator_
 	rf.fit(vec.transform(train['tweet']), train['target'])
 	rf_y_pred = rf.predict(vec.transform(validation['tweet']))
 
@@ -75,21 +99,33 @@ def RF(train, validation, test, vec):
 	)
 
 
-def GBDT(train, validation, test, vec):
-	params = {
-		'n_estimators': np.arange(0, 1001),
-		'min_samples_leaf': np.linspace(0, 20, 101),
-		'learning_rate': np.arange(0, 101, 0.1)
-	}
+# Optimum Parameters:
+# 	n_estimators: 58
+# 	min_samples_leaf: 11
+# 	learning_rate: 0.8
+def GBDT(train, validation, vec):
+	"""
+	Used to optimize and train a Gradient Boosted Decision Tree. Commented out code was previously used for parameter optimization.
+	:param train: Training DataFrame
+	:param validation: Validation DataFrame
+	:param vec: Vectorizer for tokenizing tweets
+	:return: None
+	"""
+	# params = {
+	# 	'n_estimators': np.arange(50, 61),
+	# 	'min_samples_leaf': np.arange(7, 12),
+	# 	'learning_rate': np.arange(0.8, 1.1, 0.1)
+	# }
 
-	gbdt = GradientBoostingClassifier(random_state=0)
-	gs = GridSearchCV(gbdt, params, n_jobs=n_jobs)
-	print('\nOptimizing GBDT...')
-	gs.fit(vec.transform(train['tweet']), train['target'])
-	print(gs.best_params_)
-	print(gs.best_score_)
+	gbdt = GradientBoostingClassifier(n_estimators=58, min_samples_leaf=11, learning_rate=0.8)
+	# rs = RandomizedSearchCV(gbdt, params, n_jobs=n_jobs, n_iter=50)
+	# print('\nOptimizing GBDT...')
+	# rs.fit(vec.transform(train['tweet']), train['target'])
+	# print(rs.best_params_)
+	# print(rs.best_score_)
+	# print(rs.cv_results_)
 
-	gbdt = gs.best_estimator_
+	# gbdt = rs.best_estimator_
 	gbdt.fit(vec.transform(train['tweet']), train['target'])
 	gbdt_y_pred = gbdt.predict(vec.transform(validation['tweet']))
 
@@ -100,20 +136,30 @@ def GBDT(train, validation, test, vec):
 	)
 
 
-def SVM(train, validation, test, vec):
-	params = {
-		'kernel': ['linear', 'polynomial', 'sigmoid'],
-		'C': np.linspace(0, 100, 1001)
-	}
+# Optimum Parameters:
+# 	kernel: linear
+# 	C: 2.0
+def SVM(train, validation, vec):
+	"""
+	Used to optimize and train an SVC. Commented out code was previously used for parameter optimization.
+	:param train: Training DataFrame
+	:param validation: Validation DataFrame
+	:param vec: Vectorizer for tokenizing tweets
+	:return: None
+	"""
+	# params = {
+	# 	'kernel': ['linear', 'poly', 'sigmoid'],
+	# 	'C': np.linspace(0, 100, 51)
+	# }
 
-	svm = SVC(random_state=0)
-	gs = GridSearchCV(svm, params, n_jobs=n_jobs)
-	print('\nOptimizing SVC...')
-	gs.fit(vec.transform(train['tweet']), train['target'])
-	print(gs.best_params_)
-	print(gs.best_score_)
+	svm = SVC(C=2, kernel='linear')
+	# gs = GridSearchCV(svm, params, n_jobs=n_jobs)
+	# print('\nOptimizing SVC...')
+	# gs.fit(vec.transform(train['tweet']), train['target'])
+	# print(gs.best_params_)
+	# print(gs.best_score_)
 
-	svm = gs.best_estimator_
+	# svm = gs.best_estimator_
 	svm.fit(vec.transform(train['tweet']), train['target'])
 	svm_y_pred = svm.predict(vec.transform(validation['tweet']))
 
@@ -126,15 +172,13 @@ def SVM(train, validation, test, vec):
 
 if __name__ == "__main__":
 	# Load data files
-	df_train, df_validation, df_test = load_files()
+	df_train, df_validation = load_files()
 
 	# Create corpus from all tweets
 	corpus = []
 	for tweet in df_train['tweet']:
 		corpus.append(tweet)
 	for tweet in df_validation['tweet']:
-		corpus.append(tweet)
-	for tweet in df_test['tweet']:
 		corpus.append(tweet)
 
 	# Preprocess and tokenize corpus of tweets
@@ -144,9 +188,8 @@ if __name__ == "__main__":
 	# Transform labels into numeric; real==1, fake==0
 	df_train['target'] = where(df_train['label'] == 'real', 1, 0)
 	df_validation['target'] = where(df_validation['label'] == 'real', 1, 0)
-	df_test['target'] = where(df_test['label'] == 'real', 1, 0)
 
-	NB(df_train, df_validation, df_test, vec)
-	RF(df_train, df_validation, df_test, vec)
-	GBDT(df_train, df_validation, df_test, vec)
-	SVM(df_train, df_validation, df_test, vec)
+	NB(df_train, df_validation, vec)
+	RF(df_train, df_validation, vec)
+	GBDT(df_train, df_validation, vec)
+	SVM(df_train, df_validation, vec)
